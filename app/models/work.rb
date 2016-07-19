c06b158a25cf0d6be3fd46c0e82eef312b733db3
seguelador <matias.seguel.estay@gmail.com>
2016-07-18 21:23:38 -0400

    Cascadas

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ActiveRecord::Base
   belongs_to :user
-  has_one :inventory
+  has_one :inventory, dependent: :destroy
 
   scope :last_5, -> { order(:id => :desc).limit(5) }
   scope :inventory_null, -> { where(inventory_id: nil)}
