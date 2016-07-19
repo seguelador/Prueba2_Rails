615ddfb5a7f3792d361f9e8bfe1177ec1d597f1d
seguelador <matias.seguel.estay@gmail.com>
2016-07-18 21:43:32 -0400

    callbacks

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,6 +2,12 @@ class Inventory < ActiveRecord::Base
   belongs_to :category
   belongs_to :work
   validates :wheel, numericality: { greater_than: 0 }
+  after_destroy :write_register
 
   scope :size_x, -> (x) { where("wheel > ?", x)}
+
+  def write_register
+    Register.create(description: "La pieza con serial #{self.serial} ha sido borrada")
+  end
+
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ActiveRecord::Base
   belongs_to :user
-  has_one :inventory, dependent: :destroy
+  has_one :inventory
 
   scope :last_5, -> { order(:id => :desc).limit(5) }
   scope :inventory_null, -> { where(inventory_id: nil)}
